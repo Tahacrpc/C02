c8c9d6bb329f29275862029a92d79af0c5df0be2
Tahacrpc <tahacirpici3@gmail.com>
2024-09-06 06:59:26 +0300

    Add files via upload

---
+++ C02/ex10/ft_strlcpy.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tcirpici <tcirpici@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/22 14:47:04 by tcirpici          #+#    #+#             */
+/*   Updated: 2024/08/23 20:03:58 by tcirpici         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+unsigned int	ft_strlcpy(char *dest, const char *src, unsigned int size)
+{
+	unsigned int	i;
+	unsigned int	j;
+
+	i = 0;
+	j = 0;
+	while (src[j] != '\0')
+		j++;
+	if (size == 0)
+		return (j);
+	while (i < size - 1 && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (j);
+}
